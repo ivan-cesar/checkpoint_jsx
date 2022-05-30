4fcb03d42b447620945a25000407dd3042b128ca
ivan-cesar <epinzagnetresorivan@gmail.com>
2022-05-30 17:00:21 +0000

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.png'
 import './App.css';
+import './Style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:"solid 1px black",maxWidth:"100vw",textAlign:"center"}}>
+      <h1 className='title red'><span style={{color:"#000"}}>EPINZAGNE</span> <span style={{color:"#000"}}>I</span>van <span style={{color:"#000"}}>C</span>esar</h1>
+      <br/>
+      <img src={imageInSrc}/>
+      <br/>
+      <img src='imageInPublic.png'/>
+      <br/>
+      <video width="320" height="240" controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
